28208ddf7ba4d180a252b687f48eb21f39c1f29f
Erica <ericac2700@tuckahoeschools.org>
2024-12-05 13:29:55 +0000

    no thx

---
+++ script.js
@@ -0,0 +1,5 @@
+const leftButton = document.getElementById("left-button");
+const rightButton = document.getElementById("right-button");
+const addButton = document.getElementById("add-button");
+const input = document.getElementById("input-container");
+
