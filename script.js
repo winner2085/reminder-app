cfe0304b3481133e063b0a6d5e477556b0f728da
Erica <ericac2700@tuckahoeschools.org>
2024-12-06 13:31:19 +0000

    kk

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ const leftButton = document.getElementById("left-button");
 const rightButton = document.getElementById("right-button");
 const addButton = document.getElementById("add-button");
 const input = document.getElementById("input-container");
+const container = document.querySelector('container');
 
+const inputArray = [];
+
+let storeInput = () =>{
+    addButton.addEventListener('click', () =>{
+        inputArray.push(input.value);
+    });
+}
+
+storeInput ();
